248c2269b33782dbc3cb7ed91684de415600b960
wallyflops <waller.jc@gmail.com>
2013-09-04 16:41:51 +0100

    Changed the loading lib's function.
    Don't think it's working still, seems like qv isn't happy with d3. Sadly
    I think this is all I can get done today, stuck on one very annoying
    problem- really hoping you can fix this evening so I can build some nice
    graphs in d3.js tomorrow.

--- Script.js
+++ Script.js
@@ -1,68 +1,28 @@
 var template_path = Qva.Remote + "?public=only&name=Extensions/jamesExt/";
-function extension_Init()
-{
-	//This is copied code- may be faulty but all it should do is load d3.js then run extension_Done
-	if (typeof jQuery == 'undefined') {
-	    Qva.LoadScript(template_path + 'd3.js', extension_Done);
-	}
-	else {
-	    extension_Done();
-	}        
-    
-    //If more than one script is needed you can nest the calls to get them loaded in the correct order
-    //Qva.LoadScript(template_path + "file1.js", function() {
-    //Qva.LoadScript(template_path + "file2.js", extension_Done);
-    //});
 
+function extension_Init() {
+if( typeof jQuery == 'undefined') {
+	Qva.LoadScript('/QvAjaxZfc/QvsViewClient.aspx?public=only&name=Extensions/jamesExt/jquery.js', function() {
+		Qva.LoadScript('/QvAjaxZfc/QvsViewClient.aspx?public=only&name=Extensions/jamesExt/d3.js', function() {
+			Qva.LoadScript('/QvAjaxZfc/QvsViewClient.aspx?public=only&name=Extensions/jamesExt/d3.layout.cloud.js', extension_Done);
+		});
+	});
+} else {
+	Qva.LoadScript('/QvAjaxZfc/QvsViewClient.aspx?public=only&name=Extensions/jamesExt/d3.js', function() {
+		Qva.LoadScript('/QvAjaxZfc/QvsViewClient.aspx?public=only&name=Extensions/jamesExt/d3.layout.cloud.js', extension_Done);
+	});
 }
-	//UNCOMMENT THIS BLOCK OF CODE TO ENABLE SELECT BOXES IN PROPERTIES
-	if (Qva.Mgr.mySelect == undefined) {
-    Qva.Mgr.mySelect = function (owner, elem, name, prefix) {
-        if (!Qva.MgrSplit(this, name, prefix)) return;
-        owner.AddManager(this);
-        this.Element = elem;
-        this.ByValue = true;
- 
-        elem.binderid = owner.binderid;
-        elem.Name = this.Name;
- 
-        elem.onchange = Qva.Mgr.mySelect.OnChange;
-        elem.onclick = Qva.CancelBubble;
-    }
-    Qva.Mgr.mySelect.OnChange = function () {
-        var binder = Qva.GetBinder(this.binderid);
-        if (!binder.Enabled) return;
-        if (this.selectedIndex < 0) return;
-        var opt = this.options[this.selectedIndex];
-        binder.Set(this.Name, 'text', opt.value, true);
-    }
-    Qva.Mgr.mySelect.prototype.Paint = function (mode, node) {
-        this.Touched = true;
-        var element = this.Element;
-        var currentValue = node.getAttribute("value");
-        if (currentValue == null) currentValue = "";
-        var optlen = element.options.length;
-        element.disabled = mode != 'e';
-        //element.value = currentValue;
-        for (var ix = 0; ix < optlen; ++ix) {
-            if (element.options[ix].value === currentValue) {
-                element.selectedIndex = ix;
-            }
-        }
-        element.style.display = Qva.MgrGetDisplayFromMode(this, mode);
- 
-    }
 }
 function extension_Done(){
 	Qva.AddExtension('jamesExt', function(){
-		Qva.LoadCSS(template_path + "style.css");
+		//Qva.LoadCSS(template_path + "style.css");
 		var _this = this;
 		
 		//get first text box
-		var text1 = _this.Layout.Text0.text.toString();
+		//var text1 = _this.Layout.Text0.text.toString();
 		//get check box value
-		var checkbox1 = _this.Layout.Text1.text.toString();
-		var select = _this.Layout.Text2.text.toString();
+		//var checkbox1 = _this.Layout.Text1.text.toString();
+		//var select = _this.Layout.Text2.text.toString();
 		
 		//add a unique name to the extension in order to prevent conflicts with other extensions.
 		//basically, take the object ID and add it to a DIV
@@ -82,7 +42,7 @@ function extension_Done(){
 		
 		//The below line is where are problem is- this isn't getting displayed in any way, shape or form and I'm
 		//very unsure how to debug.
-	   d3.select("#" + divName).append("svg").attr("width", _this.GetWidth()).attr("height", _this.GetHeight()).data(_this_Data).enter().append("rect").attr("x",1).attr("y",25);
+	   //d3.select("#" + divName).append("svg").attr("width", _this.GetWidth()).attr("height", _this.GetHeight()).data(_this_Data).enter().append("rect").attr("x",1).attr("y",25);
 		
 	    
 	});
