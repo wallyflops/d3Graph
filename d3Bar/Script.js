05e45f180e6a6020044b46a01fe8e6a7c33dae06
ajlandry <andrewjlandry@yahoo.com>
2013-09-09 08:54:59 -0500

    Cleaned up d3Bar/Script
    Made the loading js libraries and extension names easier to read.

--- d3Bar/Script.js
+++ d3Bar/Script.js
@@ -1,90 +1,37 @@
 //AJL: will need to make sure this is updated whenever the final extension is packaged.  I.E. the folder name will need to be whatever the final product name will be
-var template_path = Qva.Remote + "?public=only&name=Extensions/d3Bar/";
+
+var extensionName = "d3Bar";
+
+var extensionPath = Qva.Remote + "?public=only&name=Extensions/" + extensionName +"/";
 
 
-function extension_Init()
-{
-	//This is copied code- may be faulty but all it should do is load d3.js then run extension_Done
+//Function : extension_Init
+// Loading of external JS libraries
+function extension_Init(){
 
-  
-	//jQuery not defined, so load the jQuery.js
+	var files = [];
+	//if Can't resolve jQuery, then load it
 	if (typeof jQuery == 'undefined') {
-	
-	   	Qva.LoadScript(template_path+'jquery.js',function() {
-	   		Qva.LoadScript(template_path+'json2.js',function() {
-				Qva.LoadScript(template_path+'d3.v3.min.js', extension_Done());
-			
-			
-
-
-			
-			});
-		});
+	files.push(extensionPath + "jquery.js");
 	}
-	else {		
 
-	   
-	   		Qva.LoadScript(template_path+'json2.js',function() {
-	   				Qva.LoadScript(template_path+'d3.v3.min.js', extension_Done());
+	//Pushing js file names into files array
+	files.push(extensionPath + "json2.js");
+	files.push(extensionPath + "d3.v3.min.js");
 
-	   	
-	   		
-		});
-	}        
-    	
 
-    //If more than one script is needed you can nest the calls to get them loaded in the correct order
-    //Qva.LoadScript(template_path + "file1.js", function() {
-    //Qva.LoadScript(template_path + "file2.js", extension_Done);
-    //});
-
+	//loading all the js files and then executing extension_Done
+	 Qv.LoadExtensionScripts(files, extension_Done);
 }
-	//UNCOMMENT THIS BLOCK OF CODE TO ENABLE SELECT BOXES IN PROPERTIES
-	
-	if (Qva.Mgr.mySelect == undefined) {
-    Qva.Mgr.mySelect = function (owner, elem, name, prefix) {
-        if (!Qva.MgrSplit(this, name, prefix)) return;
-        owner.AddManager(this);
-        this.Element = elem;
-        this.ByValue = true;
- 
-        elem.binderid = owner.binderid;
-        elem.Name = this.Name;
- 
-        elem.onchange = Qva.Mgr.mySelect.OnChange;
-        elem.onclick = Qva.CancelBubble;
-    }
-    Qva.Mgr.mySelect.OnChange = function () {
-        var binder = Qva.GetBinder(this.binderid);
-        if (!binder.Enabled) return;
-        if (this.selectedIndex < 0) return;
-        var opt = this.options[this.selectedIndex];
-        binder.Set(this.Name, 'text', opt.value, true);
-    }
-    Qva.Mgr.mySelect.prototype.Paint = function (mode, node) {
-        this.Touched = true;
-        var element = this.Element;
-        var currentValue = node.getAttribute("value");
-        if (currentValue == null) currentValue = "";
-        var optlen = element.options.length;
-        element.disabled = mode != 'e';
-        //element.value = currentValue;
-        for (var ix = 0; ix < optlen; ++ix) {
-            if (element.options[ix].value === currentValue) {
-                element.selectedIndex = ix;
-            }
-        }
-        element.style.display = Qva.MgrGetDisplayFromMode(this, mode);
- 
-    }
-}
+
+
 
 
 //Beginning of function extension_Done. Called once the javascripts are done loading in the document
 
 function extension_Done() {
-	Qva.AddExtension('d3Bar', function(){
-		Qva.LoadCSS(template_path + "style.css");
+	Qva.AddExtension(extensionName, function(){
+		Qva.LoadCSS(extensionPath + "style.css");
 		var _this = this;
 		
 		//get first text box
@@ -190,19 +137,12 @@ function extension_Done() {
 		    .text(String);
 
 
-		//The below line is where are problem is- this isn't getting displayed in any way, shape or form and I'm
-		//very unsure how to debug.
-	//d3.select("#" + divName).append("svg").attr("width", _this.GetWidth()).attr("height", _this.GetHeight()).data(data).enter().append("rect").attr("x",1).attr("y",25);
 
 
 	    
 	});
 }
 //Initiate extension
-	//AJL: Embeds Firebug Lite to the qlikview screen which should be very helpful. Will need to remove once done
-
- //Qva.LoadScript('https://getfirebug.com/firebug-lite.js', function(){
-
 
 extension_Init();
- //	});
+
